80a12d606a4414b151f848f1ef83e0010d6f1143
Isaac Kim <isaacmmikim@gmail.com>
2021-11-01 16:52:09 -0700

    Fixed MainPage issues.

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,29 +1,23 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
-import {Container} from 'react-bootstrap'
-import {Button} from 'react-bootstrap'
-import {Row, Col} from 'react-bootstrap'
-import '../FoodCard.css'
-function FoodCards(props){
-  return(
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import "./FoodCard.css";
+
+function FoodCards(props) {
+  return (
     <>
-              <Card className ="CardStyle"  >
-                         <Card.Img variant="top" className= "imageStyle" src ={props.image} />
-                           <Card.Body>
-                             <Card.Title>{props.title}</Card.Title>
-                            <Card.Text>
-                                Eat eat eat eat eat eat eat eat eat
-                             </Card.Text>
-                             <Card.Footer>
-                                 Star Rating
-                             </Card.Footer>
-                        </Card.Body>
-                           </Card>
-  
-         
-   
+      <Card className="CardStyle">
+        <Card.Img variant="top" className="imageStyle" src={props.image} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>Eat eat eat eat eat eat eat eat eat</Card.Text>
+          <Card.Footer>Star Rating</Card.Footer>
+        </Card.Body>
+      </Card>
     </>
-  )
+  );
 }
 
 export default FoodCards;
