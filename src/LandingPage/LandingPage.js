4e5c78f73cebf503330be351d8ac28813f12d087
Isaac Kim <isaacmmikim@gmail.com>
2021-11-01 16:26:34 -0700

    Minor change

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,24 +1,10 @@
-import {
-  Button,
-  Row,
-  Col,
-  FloatingLabel,
-  Form,
-  Container,
-} from "../../node_modules/react-bootstrap";
+import { Container } from "../../node_modules/react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import "./LandingPage.css";
-import { useHistory } from "react-router-dom";
 
 const LandingPage = () => {
-  let history = useHistory();
-
-  const handleClick = () => {
-    history.push("/main-page");
-  };
-
   return (
     <Container>
       <Header />
